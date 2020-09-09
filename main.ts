81d70b4607c02db482d49e6e60073c59661d96bd
themarkness <williamsmarka@gmail.com>
2020-09-09 08:38:34 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Yes)
 })
+input.onGesture(Gesture.Shake, function () {
+    if (input.runningTime() == 0) {
+    	
+    }
+})
